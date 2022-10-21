c0fff1c7e7ac39c97c89aeb60b2877797ac5948c
youssef <yyasser849@gmail.com>
2022-10-21 18:56:48 +0200

    icons error

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -73,7 +73,7 @@ class RecipeView extends View {
 
     <div class="recipe__user-generated ${this._data.key ? '' : 'hidden'}">
       <svg>
-        <use href="${icons}}#icon-user"></use>
+        <use href="${icons}#icon-user"></use>
       </svg>
     </div>
 
